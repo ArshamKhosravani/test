67d777081670afb68eae627c243aa9a04f2edbb6
Arsham <mmr.arsham@gmail.com>
2021-02-05 21:00:28 +0330

    jwt final

---
+++ src/main/java/com/arsham/test/demo/Config/UserPassAuthRequest.java
@@ -0,0 +1,14 @@
+package com.arsham.test.demo.Config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserPassAuthRequest {
+
+    private String username;
+    private String password;
+}
